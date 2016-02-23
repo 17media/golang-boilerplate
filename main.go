94c044d321b83c33a1fb628f2fdd8e6792e55173
Sam Liu <sam@17.media>
2016-02-23 15:39:54 +0800

    switch to godep

--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"github.com/codegangsta/negroni"
-	"github.com/dimfeld/httptreemux.git"
-	"net/http"
+    "fmt"
+    "github.com/codegangsta/negroni"
+    "github.com/julienschmidt/httprouter"
+    "net/http"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+}
+
 // Handler
-func hello(c *echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!\n")
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func main() {
-	router = httptreemux.New()
-	api := router.NewGroup("/api/v1")
-	api.GET("/foo", hello) // becomes /api/v1/foo
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:name", Hello)
 
-	// Middleware
-	n := negroni.Classic()
-	n.UseHandler(router)
+    // Middleware
+    n := negroni.Classic()
+    n.UseHandler(router)
 
-	// Start server
-	n.Run(":3000")
+    // Start server
+    n.Run(":3000")
 }
