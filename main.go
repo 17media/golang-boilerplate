fe3994c97967f070b604983690040e402197bbef
Sam Liu <genxstylez@gmail.com>
2016-03-09 17:29:41 +0800

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-        "os"
-        "fmt"
-        "github.com/codegangsta/negroni"
-        "github.com/julienschmidt/httprouter"
-        "net/http"
-        "github.com/facebookgo/grace/gracehttp"
-        config "github.com/spf13/viper"
+	"fmt"
+	"github.com/codegangsta/negroni"
+	"github.com/facebookgo/grace/gracehttp"
+	"github.com/julienschmidt/httprouter"
+	config "github.com/spf13/viper"
+	"net/http"
+	"os"
 )
 
 // Index function
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-        fmt.Fprint(w, "WelWWWW111come1111!\n")
+	fmt.Fprint(w, "WelWWWW111come1111!\n")
 }
 
 // Hello function
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-        fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func main() {
-        router := httprouter.New()
-        router.GET("/", Index)
-        router.GET("/hello/:name", Hello)
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
 
-        User.hashPassword
-        // Middleware
-        n := negroni.Classic()
-        n.UseHandler(router)
+	User.hashPassword
+	// Middleware
+	n := negroni.Classic()
+	n.UseHandler(router)
 
-        // Start server
-        gracehttp.Serve(
-                &http.Server{
-                        Addr: ":" + os.Getenv("PORT"),
-                        Handler: n,
-                },
-        )
+	// Start server
+	gracehttp.Serve(
+		&http.Server{
+			Addr:    ":" + os.Getenv("PORT"),
+			Handler: n,
+		},
+	)
+
 }
